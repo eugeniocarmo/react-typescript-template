88405c1d12169e23026390447bae5019c28ff10c
eugeniocarmo <eugeniocarmo@gmail.com>
2022-12-23 14:21:05 +0000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello from Eugenio</h1>
+      <p>Initial code to use React and TypeScript</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-typescript-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-obbnju)
